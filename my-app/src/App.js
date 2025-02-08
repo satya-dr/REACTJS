f9a6c6ff2edc448c790121ab0488f296fe03247a
SATYAJIT ROY <satyajitroy19599@gmsil.com>
2025-02-08 14:52:21 +0530

    All complete now we are create a  basic web in another branch 'basicPro'

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
+let name="satyajit";
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return(
+  <>
+  <nav>
+    <li>Home</li>
+    <li>Contact</li>
+    <li>About</li>
+  </nav>
+  <div className="container">
+    <h1>Hello {name}</h1>
+    <p>    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quod, possimus minima voluptas dolorem cum eaque. Omnis libero minus tempora enim?
+    </p>
+  </div>
+  </>
+); 
 }
+
 
 export default App;
