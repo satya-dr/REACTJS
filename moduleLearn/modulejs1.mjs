14d170b3c97f9997c7df9bbcb85cea8a140774cb
SATYAJIT ROY <satyajitroy19599@gmsil.com>
2025-02-09 14:41:59 +0530

    Only for learn Parpuses

---
+++ moduleLearn/modulejs1.mjs
@@ -0,0 +1,5 @@
+import c,{a,b,d} from './modulejs2.mjs'
+console.log(c);
+console.log(b);
+console.log(c);
+console.log(d);
---
+++ moduleLearn/modulejs2.mjs
@@ -0,0 +1,9 @@
+const a="Satyajit";
+const b= "Debojit";
+const c= "Subham";
+const d = "Ayan";
+
+export default c;
+export {a};
+export {b};
+export {d};
