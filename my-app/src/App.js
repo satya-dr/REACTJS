c57078f748e12df49484e2345ad3295746cb981c
SATYAJIT ROY <satyajitroy19599@gmsil.com>
2025-02-20 14:19:15 +0530

    Add dark mode

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,18 +1,32 @@
 // import logo from './logo.svg';
 import './App.css';
+// import About from './components/About';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
+import React,{useState} from 'react'
 
 function App() {
+  const [mode, setMode] = useState('light');
+  const toggleMode= ()=>{
+    if(mode=== 'light'){
+      setMode('dark');
+      document.body.style.backgroundColor='#042743';
+    }
+    else{
+      setMode('light');
+      document.body.style.backgroundColor='white';
+    }
 
+  }
   return (
    
     <>
 
-       { <Navbar title="CGEC TIMES" aboutText="About CGEC" />}
+       { <Navbar title="CGEC TIMES" aboutText="About CGEC" mode={mode} toggleMode={toggleMode} />}
        {/* <Navbar /> */}
         <div className="container my-3">
-        <TextForm heading="Enter the text to analyze below"/>
+        <TextForm heading="Enter the text to analyze below" mode={mode}/>
+         {/* <About/> */}
         </div>
     </>
     
--- my-app/src/components/TextForm.js
+++ my-app/src/components/TextForm.js
@@ -9,24 +9,65 @@ export default function TextForm(props) {
         // setText("You are clicked on handelUpclick");
         setText(newText);
      }
+     const handelLoclick = ()=>{
+      // console.log("Upper Case Was Clicked" + text);
+      let newText = text.toLowerCase();
+      // setText("You are clicked on handelUpclick");
+      setText(newText);
+   }
+   const handelClear = ()=>{
+    // console.log("Upper Case Was Clicked" + text);
+    // let newText = text(' ');
+    // setText("You are clicked on handelUpclick");
+    setText(' ');
+ }
+ const handelSubmit = ()=>{
+  // console.log("Upper Case Was Clicked" + text);
+    //  let conf= confirm("Are you sure ?");
+  // setText("You are clicked on handelUpclick");
+     alert(" Your Text is Submited! \n Thank You.....!");
+}
      
      const handelOnChange = (event)=>{
         // console.log("On Change");
         setText(event.target.value)
      }
+     const handelCopy = ()=>{
+      var text =document.querySelector("#myBox");
+      text.select();
+      navigator.clipboard.writeText(text.value);
+     }
+     const handleExtraSpaces=()=>{
+      let newText=text.split(/[ ]+/);
+      setText(newText.join(" "));
+     }
 
-
-    const [text, setText]=useState('Enter text here');
+    const [text, setText]=useState('');
     // text ="New Text"// Worng way to change state
     // setText("New Text");// Correct way to change state
   return (
-    <div>
+    <>
+    <div className="container" style={{color: props.mode === 'dark'? 'white':'#042743'}}>
         <h1>{props.heading}  </h1>
         <div className="mb-3">
-          <textarea className="form-control" value={text} onChange={handelOnChange} id="myBox" rows="8"></textarea>
+          <textarea className="form-control" value={text} onChange={handelOnChange} style={{backgroundColor : props.mode === 'dark'? 'grey':'white',color: props.mode === 'dark'? 'white':'#042743'}} id="myBox" rows="8"></textarea>
         </div>
-        <button className="btn btn-primary" onClick={handelUpclick}>Convert to UpporCase</button>
+        <button className="btn btn-primary mx-1" onClick={handelUpclick}>Convert to UpporCase</button>
+        <button className="btn btn-primary mx-1" onClick={handelLoclick}>Convert to UpporCase</button>
+        <button className="btn btn-primary mx-1" onClick={handelClear}>Clear</button>
+        <button className="btn btn-primary mx-1" onClick={handelSubmit}>Submit</button>
+        <button className="btn btn-primary mx-1" onClick={handelCopy}>Copy Text</button>
+        <button className="btn btn-primary mx-1" onClick={handleExtraSpaces}>Remove Extra Space</button>
 
     </div>
+    <div className="containermy-2" style={{color: props.mode === 'dark'? 'white':'#042743'}}>
+      <h1>Yor Text Summary</h1>
+      <p> {text.split(" ").length} Words and {text.length} characters</p>
+      <p> {0.008 * text.split(" ").length} minutes read </p>
+      <h2>Preview</h2>
+      <p>{text.length>0?text:"Enter Something in the textbox above to preview it here!"}</p>
+    </div>
+
+    </>
   )
 }
