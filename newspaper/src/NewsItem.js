43c5215f1872d3402de5ba84b80ceb2efa1041eb
SATYAJIT ROY <satyajitroy19599@gmail.com>
2025-02-27 14:45:41 +0530

    Many feature added

--- newspaper/src/NewsItem.js
+++ newspaper/src/NewsItem.js
@@ -9,11 +9,11 @@ export class NewsItem extends Component {
     return (
       <div className='my-3'>
           <div className="card" style={{width: "18rem"}}>
-               <img src={imageUrl} className="card-img-top" alt="..."/>
+               <img src={!imageUrl?"https://www.ft.com/__origami/service/image/v2/images/raw/https%3A%2F%2Fd1e00ek4ebabms.cloudfront.net%2Fproduction%2F83665594-89ac-40a2-92f8-f96a5a4da0da.jpg?source=next-barrier-page":imageUrl } className="card-img-top" alt="..."/>
                <div className="card-body">
                  <h5 className="card-title">{title}....</h5>
                  <p className="card-text">{description}....</p>
-                 <a href={newsUrl} target='_blank' className="btn btn-sm btn-primary">Read more</a>
+                 <a rel="noreferrer" href={newsUrl} target='_blank' className="btn btn-sm btn-dark">Read more</a>
                </div>
              </div>
           </div>
