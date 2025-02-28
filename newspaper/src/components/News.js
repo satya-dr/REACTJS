6c02b57bb2531dc295a6737c879cfe4cbb4e9ba4
SATYAJIT ROY <satyajitroy19599@gmail.com>
2025-02-28 14:53:41 +0530

    Some changes

--- newspaper/src/components/News.js
+++ newspaper/src/components/News.js
@@ -78,7 +78,7 @@ export class News extends Component {
   render() {
     return (
       <div className="container my-3">
-        <h1 className='text-center'>CGEC-TIMES - Top Headlines...!</h1>
+        <h1 className='text-center'style={{margin:'30px 0px'}}>CGEC-TIMES - Top Headlines...!</h1>
         {this.state.loading && <Spinner/>}
         <div className="row">
           {!this.state.loading && this.state.articles.map((element) => {
@@ -89,6 +89,9 @@ export class News extends Component {
                   description={element.description?.slice(0, 88) || 'No Description Available'}
                   newsUrl={element.url}
                   imageUrl={element.urlToImage}
+                  author={element.author}
+                   date={element.publishedAt}
+                   source={element.source.name}
                 />
               </div>
             );
